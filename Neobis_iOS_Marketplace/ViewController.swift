efbf1415ac7f3db78f55bb291830ba14f4df6056
F4rab1 <issafarabi2022@gmail.com>
2023-06-12 17:57:05 +0600

    Initial Commit

---
+++ Neobis_iOS_Marketplace/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Neobis_iOS_Marketplace
+//
+//  Created by Фараби Иса on 12.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
